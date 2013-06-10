f886e5e892cdc713df2f5701225017e92f69fb5c
Gabriel <gkind@hs-mittweida.de>
2013-06-10 16:50:56 +0200

    Fix permission check. Mix Species with Item permissions when an item is requested.

--- cyanofactory/cyano/decorators.py
+++ cyanofactory/cyano/decorators.py
@@ -72,18 +72,18 @@ def permission_required(permission):
             perm = models.Permission.objects.get(name = permission)
             
             profile = user.profile
-            
-            check_species = True
+
+            allow_item = True
+            deny_item = False
+
             if item:
-                allow, deny = profile.has_permission(item, perm)
-                
-                # No permission for that item, check species permissions instead
-                check_species = allow == None                                    
+                allow_item, deny_item = profile.has_permission(item, perm)
+
+            # allow or deny are not None if an item had permission assigned
+            # Mix item permissions with species permissions now
+            allow_species, deny_species = profile.has_permission(species, perm)
             
-            if check_species:
-                allow, deny = profile.has_permission(species, perm)
-            
-            if allow and not deny:
+            if (allow_species or allow_item) and not (deny_species or deny_item):
                 # Has permission
                 return function(request, *args, **kw)
             else:
@@ -95,16 +95,21 @@ def permission_required(permission):
                     extra = "DEBUG: Permissions are (allow, deny, needed):<br>"
                     
                     allow_perms, deny_perms = profile.get_permissions(species)
+                    if item:
+                        allow_perms_item, deny_perms_item = profile.get_permissions(item)
+                        allow_perms += allow_perms_item
+                        deny_perms += deny_perms_item
 
                     perm_list = [0 for x in range(8)]
-                    perm_allow = perm_list
-                    perm_deny = perm_list
-                    perm_needed = perm_list
+                    perm_allow = list(perm_list)
+                    perm_deny = list(perm_list)
+                    perm_needed = list(perm_list)
                     
                     for i in range(8):
-                        perm_allow[i] = 1 if models.Permission.get_by_pk(i + 1) in allow_perms else 0
-                        perm_deny[i] = 1 if models.Permission.get_by_pk(i + 1) in deny_perms else 0
-                        perm_needed[i] = 1 if models.Permission.get_by_pk(i + 1) == perm else 0
+                        cur_perm = models.Permission.get_by_pk(i + 1)
+                        perm_allow[i] = 1 if cur_perm in allow_perms else 0
+                        perm_deny[i] = 1 if cur_perm in deny_perms else 0
+                        perm_needed[i] = 1 if cur_perm == perm else 0
    
                     extra += "<pre>{0}</pre><pre>{1}</pre><pre>{2}</pre>".format(
                             "".join(str(x) for x in perm_allow),
