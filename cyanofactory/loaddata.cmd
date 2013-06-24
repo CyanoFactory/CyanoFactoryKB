7cda3ad656275c6630a3e0549025d208ee40d103
Gabriel <gkind@hs-mittweida.de>
2013-06-24 18:15:11 +0200

    Fix typo in loaddata (biocyc -> sbml)

--- cyanofactory/loaddata.cmd
+++ cyanofactory/loaddata.cmd
@@ -4,5 +4,5 @@ python manage.py restoredb < boehringer/fixtures/data.dump
 :: python manage.py restoredb < d:\db.dump
 python manage.py autocreateinitial
 python manage.py importbiodata ../sequences/NC_000911.gb --traceback
-python manage.py importbiocyc ../sample_data/iSyn811_v2-2_sbml_fixed.xml --traceback
+python manage.py importsbml ../sample_data/iSyn811_v2-2_sbml_fixed.xml --traceback
 python manage.py importbiodata ../sequences/NC_000913.gb --traceback
