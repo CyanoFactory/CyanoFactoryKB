f0b668791ccd7d842acc8e074f5b7ea8e334d2ef
Gabriel Kind <gkind@hs-mittweida.de>
2014-06-06 17:14:42 +0200

    Don't delete empty revisions with delete flag

--- cyanofactory/cyano/management/commands/deleteemptyrevisions.py
+++ cyanofactory/cyano/management/commands/deleteemptyrevisions.py
@@ -6,6 +6,7 @@ Released under the MIT license
 """
 
 from django.core.management.base import BaseCommand
+from django.db.models.query_utils import Q
 
 from cyano.models import Revision, RevisionDetail
 
@@ -13,7 +14,7 @@ class Command(BaseCommand):
     """Deletes empty revisions
     """
     def handle(self, *args, **options):
-        revisions = Revision.objects.filter(new_data="{}")
+        revisions = Revision.objects.filter(Q(new_data="{}") & ~Q(action="D"))
         print("Deleting {} revisions".format(revisions.count()))
         revisions.delete()
         rev_details = RevisionDetail.objects.filter(revisions__isnull=True)
