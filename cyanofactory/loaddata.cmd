c85ec0879f3b6e3e61af0f1a72e4010d60853080
Gabriel Kind <gkind@hs-mittweida.de>
2014-06-16 18:03:37 +0200

    Fix loaddata for Django 1.6

--- cyanofactory/loaddata.cmd
+++ cyanofactory/loaddata.cmd
@@ -4,7 +4,7 @@ echo Cyanofactory KB Initial Import script
 
 python manage.py syncdb
 python manage.py migrate
-python manage.py loaddata cyano/fixtures/metadata.json
+python manage.py loaddata cyano_metadata.json
 python manage.py restoredb < boehringer/fixtures/data.dump
 python manage.py autocreateinitial
 python manage.py create_meta
