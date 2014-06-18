f63ab0a8f4ed6d8404297487f10629b98790179c
Gabriel Kind <gkind@hs-mittweida.de>
2014-06-18 16:24:09 +0200

    Add additional data (SNPs, interproscan) for import.

--- cyanofactory/loaddata.cmd
+++ cyanofactory/loaddata.cmd
@@ -14,7 +14,7 @@ python manage.py import_kegg_ec
 python manage.py create_species --wid=Synechocystis --name="Synechocystis sp. PCC 6803" --reason="Create Synechocystis"
 python manage.py create_species --wid=Ecoli --name="Escherichia coli str. K-12 substr. MG1655" --reason="Create Ecoli"
 
-python manage.py importgenbank ../sequences/NC_000911_Syn.gb --wid=Synechocystis --reason="Import Chromosome of Synechocystis" --chromosome=Chromosome-Syn --name="Chromosome" --user=management
+python manage.py importgenbank ../sequences/NC_000911_Syn.gb --wid=Synechocystis --reason="Import Chromosome of Synechocystis" --chromosome=Chromosome --name="Chromosome" --user=management
 python manage.py importgenbank ../sequences/NC_005229_Syn_Plasmid_pSYSM.gb --wid=Synechocystis --reason="Import Plasmid pSYSM of Synechocystis" --chromosome=pSYSM --name="Plasmid pSYSM" --user=management
 python manage.py importgenbank ../sequences/NC_005230_Syn_Plasmid_pSYSA.gb --wid=Synechocystis --reason="Import Plasmid pSYSA of Synechocystis" --chromosome=pSYSA --name="Plasmid pSYSA" --user=management
 python manage.py importgenbank ../sequences/NC_005231_Syn_Plasmid_pSYSG.gb --wid=Synechocystis --reason="Import Plasmid pSYSG of Synechocystis" --chromosome=pSYSG --name="Plasmid pSYSG" --user=management
@@ -25,3 +25,10 @@ python manage.py importgenbank ../sequences/NC_000913_Ecoli.gb --wid=Ecoli --rea
 python manage.py importsbml ../sample_data/iSyn811_v2-2_sbml_fixed.xml --wid=Synechocystis --reason="Import SBML iSyn from Valencia Dataset" --user=management
 
 python manage.py importproopdb ../sample_data/SynOperonPrediction.txt --wid=Synechocystis --reason="Import Operon Prediction Files" --user=management
+
+python manage.py importinterproscan ../sample_data/synecho.interpro.fasta.xml --wid=Synechosystis --reason="Import InterProScan Data" --user=management
+
+python manage.py importfastafeature ../sample_data/SNPs/chromosom_2.txt --wid=Synechosystis --reason="Import Uppsala SNPs for Chromosome" --chromosome=Chromosome --feature-type=SNP --user=management
+python manage.py importfastafeature ../sample_data/SNPs/plasmid29_2.txt --wid=Synechosystis --reason="Import Uppsala SNPs for Plasmid pSYSM" --chromosome=pSYSM --feature-type=SNP --user=management
+python manage.py importfastafeature ../sample_data/SNPs/plasmid30_2.txt --wid=Synechosystis --reason="Import Uppsala SNPs for Plasmid pSYSA" --chromosome=pSYSA --feature-type=SNP --user=management
+python manage.py importfastafeature ../sample_data/SNPs/plasmid32_2.txt --wid=Synechosystis --reason="Import Uppsala SNPs for Plasmid pSYSX" --chromosome=pSYSX --feature-type=SNP --user=management
